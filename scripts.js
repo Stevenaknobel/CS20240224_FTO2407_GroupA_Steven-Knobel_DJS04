acf68f3fdf32c8e954fda436b025ff1094faacb8
Steven <stevenaknobel@gmail.com>
2024-11-01 08:14:49 +0200

    Create a function for book previews and call the new function for the initial preview

--- scripts.js
+++ scripts.js
@@ -3,7 +3,26 @@ import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 let page = 1;
 let matches = books
 
-const starting = document.createDocumentFragment()
+//Create a function to create the book preview button so you dont rewrite the code (DRY principle(Don't repeat yourself))
+function bookPreviewButton({ author, id, image, title }) {
+    const element = document.createElement('button');
+    element.classList.add('preview');
+    element.setAttribute('data-preview', id);
+    element.innerHTML = `
+        <img class="preview__image" src="${image}" />
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${authors[author]}</div>
+        </div>
+    `;
+    return element;
+}
+
+//populate the initial book preview with the new function
+const starting = document.createDocumentFragment();
+matches.slice(0, BOOKS_PER_PAGE).forEach(book => {
+    starting.appendChild(bookPreviewButton(book))
+})
 //create book preview buttons on the page
 for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
     const element = document.createElement('button')
